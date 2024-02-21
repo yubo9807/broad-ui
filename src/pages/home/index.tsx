58fbc33a9d383c2edd1d93d49c4395268ee8465f
yubo9807 <yubo9807@gmail.com>
2024-02-21 16:24:03 +0800

    fix: home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,8 +2,16 @@ import { h } from "pl-vue";
 import Markdown from "@/components/Markdown";
 import use from '~/readme.md?raw';
 import env from "~/config/env";
+import style from "./index.module.scss";
 
 export default function() {
   const text = use.replace(/~\/core/g, env.PROJECT_NAME.toLocaleLowerCase()).replace(/#.+\n/, '');
-  return <Markdown text={text} />
+  return <div>
+    <div className={style.banner}>
+      <div className={style.box}>
+        <h1>Broad-UI</h1>
+      </div>
+    </div>
+    <Markdown text={text} />
+  </div>
 }
