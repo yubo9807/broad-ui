a37e1c02e89904b0c9150ef2062785a079a94fc4
yubo9807 <yubo9807@gmail.com>
2024-03-08 18:02:27 +0800

    feat: Select component

---
+++ core/comp/Select/index.tsx
@@ -0,0 +1,25 @@
+import { h, PropsType, ref } from "pl-vue";
+import { OptionValue } from "./option";
+import './module.scss';
+
+type Props = PropsType<{
+  model?:        OptionValue | { value: OptionValue }
+  onChange?:     (val: OptionValue) => void
+  className?:    string
+}>
+export default function(props: Props) {
+
+  const model = typeof props.model === 'object' ? props.model : ref(props.model);
+  props.children.forEach(val => {
+    val.attrs.selected = () => model.value === val.attrs.value;
+  })
+
+  function handleChange(e) {
+    model.value = e.target.value;
+    props.onChange && props.onChange(model.value);
+  }
+
+  return <select className={['comp-select', props.className]} onchange={handleChange}>
+    {props.children}
+  </select>
+}
